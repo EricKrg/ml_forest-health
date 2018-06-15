d42745e3734fe99d23c9f01ca246a93f90efcb97
EricKrg <eric.krueger@uni-jena.de>
2018-06-15 11:59:39 +0200

    wfs example script

---
+++ scripts/wfs_example.R
@@ -0,0 +1,26 @@
+library(sf)
+library(dplyr)
+library(stringr)
+
+
+warnungen = function() {
+  # ogrListLayers("https://maps.dwd.de/geoserver/ows?service=wfs&version=2.0.0&request=GetCapabilities")
+  # ogrinfo(dsn, so=TRUE)
+  
+  warnungen = tryCatch({
+    ################################################################################
+    #st_read ist die funcktion welche wir brauchen um wfs und wcs services zu nutzten
+    ################################################################################
+    warnungen = st_read("WFS:https://maps.dwd.de/geoserver/ows?service=wfs&version=2.0.0&request=GetCapabilities",
+                        layer = "dwd:Warnungen_Gemeinden",
+                        quiet = T) %>%
+      st_cast("GEOMETRYCOLLECTION") %>% st_collection_extract("POLYGON") %>%
+      dplyr::select(EVENT, DESCRIPTION, SENT, PARAMATERVALUE, WEB) %>%
+      dplyr::mutate(EVENT = as.factor(stringr::str_to_title(as.character(EVENT))))
+  },
+  error=function(cond) {
+    message("No weather warnings")
+    no = TRUE
+  })
+  return(warnungen)
+}
